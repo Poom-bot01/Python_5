756734cbf8241c952c62aebac280507a64b866ae
Poom-bot01 <aranpoom9@gmail.com>
2026-09-16 10:08:06 +0700

    Update p8m.py

--- p8m.py
+++ p8m.py
@@ -1,3 +1,7 @@
 import p8f
 
-if __
+if __name__ == "__main__":
+  n, k = input("Enter n and k: ").split()
+  n = int(n); k = int(k)
+  CnK = (p8f.fact(n)) / (p8f.fact(k) * p8f.fact(n-k))
+  print(f"C({n},{k}) = {Cnk}")
